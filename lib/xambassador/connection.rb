4b7abb931a5926b4f242f384836077f43dc6d757
ratherblue <ratherblue@gmail.com>
2016-03-08 17:45:16 -0800

    fix tests

--- lib/xambassador/connection.rb
+++ lib/xambassador/connection.rb
@@ -1,3 +1,5 @@
+require 'octokit'
+
 module Xambassador
   # Connection helpers
   class Connection
--- test/unit/xambassador/connection_test.rb
+++ test/unit/xambassador/connection_test.rb
@@ -1,9 +1,9 @@
-require 'xambassador/connect'
+require 'xambassador/connection'
 
 describe Xambassador::Connection do
   subject { Xambassador::Connection }
 
   it 'should have an auth token' do
-    subject.token.wont_be_empty
+    subject.new.token.wont_be_empty
   end
 end
