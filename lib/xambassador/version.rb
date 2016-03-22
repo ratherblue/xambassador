235c6c5e233767ef913069f0eb33f3a8b5d5e5e2
ratherblue <ratherblue@gmail.com>
2016-03-22 11:10:06 -0700

    fix line length, bump version

--- lib/xambassador/version.rb
+++ lib/xambassador/version.rb
@@ -1,4 +1,4 @@
 # Xambassador version
 module Xambassador
-  VERSION = '0.0.14'.freeze
+  VERSION = '0.0.15'.freeze
 end
--- test/fake_version_one.rb
+++ test/fake_version_one.rb
@@ -4,7 +4,7 @@ require 'sinatra/base'
 class FakeVersionOne < Sinatra::Base
   set :show_exceptions, false
 
-  get "#{ENV['VERSION_ONE_URL']}/Data/Story?sel=Number&where=Number='B-51609'" do
+  get "#{ENV['VERSION_ONE_URL']}/Data/Story" do
     # get "#{ENV['VERSION_ONE_URL']}/rest-1.v1/Data/Story" do
   end
 end
